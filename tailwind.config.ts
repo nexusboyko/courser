4fec69f9dbf1c82d262e8f0103daefd27bee3e0a
nexusboyko <alexboyko@outlook.com>
2024-04-26 20:51:20 -0700

    Add DaisyUI plugin for Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ['light', 'dark']
+  }
 };
 export default config;
