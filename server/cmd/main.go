4a518927d0e534761d29d5467869226a9e90d5d8
nexusboyko <alexboyko@outlook.com>
2024-08-16 17:34:38 -0700

    Add CORS middlware and update main api route to POST

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func main() {
@@ -14,13 +15,18 @@ func main() {
 
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+	}))
 
 	router.Route("/api", func(r chi.Router) {
 		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte("main"))
 		})
 
-		r.Get("/courser", internal.NewCourserHandler().ServeHTTP)
+		r.Post("/courser", internal.NewCourserHandler().ServeHTTP)
 	})
 
 	http.ListenAndServe(`:8080`, router)
