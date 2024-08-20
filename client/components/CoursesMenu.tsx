634af7e6e3dbba9fb8cd47d6c8724792c25f11d1
nexusboyko <alexboyko@outlook.com>
2024-08-19 17:11:36 -0700

    Add small animation using Framer Motion

--- client/components/CoursesMenu.tsx
+++ client/components/CoursesMenu.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { NestedItem } from "@/app/layout";
 import React from "react";
+import { motion } from "framer-motion";
+
+import { NestedItem } from "@/lib/utils";
 
 const CoursesMenu = ({
   setJson,
@@ -60,57 +62,80 @@ const CoursesMenu = ({
   };
 
   return (
-    <section
+    <motion.section
+      initial="hidden"
+      animate="show"
+      variants={{
+        hidden: { 
+          opacity: 0, 
+          translateX: -100 
+        },
+        show: {
+          translateX: 0,
+          opacity: 1,
+          transition: {
+            type: "spring",
+            bounce: 0.3,
+            ease: "easeIn",
+          },
+        }
+      }}
       id="courses-menu"
-      className="absolute left-0 top-1/4 ml-2 border-t border-l border-r"
+      className="absolute left-0 top-1/4 ml-2"
     >
-      {Object.entries(items).map(([item, json]) => (
-        <div key={item} className="border-b flex justify-between">
-          <div className="m-0 p-0 w-[15ch] flex justify-center items-center text-center">
-            {
-              editingItem === item ? (
-                <form
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    updateItem(item, newKey);
-                    setEditingItem("");
-                  }}
-                >
-                  <small>
-                    <input
-                      type="text"
-                      value={newKey ? newKey : item}
-                      onChange={(e) => setNewKey(e.target.value)}
-                      className="p-2 w-full"
-                      autoFocus
-                    />
+      <div className="flex flex-col justify-center items-center p-2 text-base">
+        <p className="text-2xl">🗃️</p>
+        <p className="text-center text-base font-semibold">my courses</p>
+      </div>
+      <div className="border-t border-l border-r rounded-lg">
+        {Object.entries(items).map(([item, json]) => (
+          <div key={item} className="border-b rounded-b-lg flex justify-between">
+            <div className="m-0 p-0 w-[15ch] flex justify-center items-center text-center hover:bg-gray-200">
+              {
+                editingItem === item ? (
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      updateItem(item, newKey);
+                      setEditingItem("");
+                    }}
+                  >
+                    <small>
+                      <input
+                        type="text"
+                        value={newKey ? newKey : item}
+                        onChange={(e) => setNewKey(e.target.value)}
+                        className="p-2 w-full"
+                        autoFocus
+                      />
+                    </small>
+                  </form>
+                ) : (
+                  <small
+                    className="p-2 w-full cursor-pointer"
+                    onClick={() => setJson(json as NestedItem)}
+                  >
+                    {item}
                   </small>
-                </form>
-              ) : (
-                <small
-                  className="p-2 w-full hover:bg-gray-200 cursor-pointer"
-                  onClick={() => setJson(json as NestedItem)}
-                >
-                  {item}
-                </small>
-              )
-            }
+                )
+              }
+            </div>
+            <button
+              className="border-l p-2 hover:bg-red-200 cursor-pointer"
+              onClick={() => deleteItem(item)}
+            >
+              🗑️
+            </button>
+            <button
+              className="border-l p-2 hover:bg-green-200 cursor-pointer"
+              onClick={() => setEditingItem(item)}
+            >
+              ✒️
+            </button>
           </div>
-          <button
-            className="border-l p-2 hover:bg-red-200 cursor-pointer"
-            onClick={() => deleteItem(item)}
-          >
-            🗑️
-          </button>
-          <button
-            className="border-l p-2 hover:bg-green-200 cursor-pointer"
-            onClick={() => setEditingItem(item)}
-          >
-            ✒️
-          </button>
-        </div>
-      ))}
-    </section>
+        ))}
+      </div>
+    </motion.section>
   );
 };
 
