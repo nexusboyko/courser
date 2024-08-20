f148d82d35e2856e747eaecf9b06d4913ab0c266
nexusboyko <alexboyko@outlook.com>
2024-08-20 11:46:27 -0700

    Setup next config for build

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/courser",
+  output: "export",
+  reactStrictMode: true,
+
+  env: {
+    NEXT_ENV: "production",
+    NEXT_API_URL_PROD: "http://165.1.75.49:8080",
+    NEXT_API_URL_DEV: "http://localhost:8080",
+  }
+};
 
 export default nextConfig;
